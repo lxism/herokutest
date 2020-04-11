3a3f694d405e60f0f9f5236d27eea6b1536bacfb
lxism <lxism@users.noreply.github.com>
2020-04-11 13:47:23 -0500

    static url

--- test_project/settings/production.py
+++ test_project/settings/production.py
@@ -123,7 +123,7 @@ COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static'
 
 
 # Authentication
